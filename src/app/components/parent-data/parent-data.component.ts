d784cac8465d63025a330ca696e5bf35769b361e
Rafael Franca <rafa-cross@hotmail.com>
2024-05-14 13:48:43 -0300

    routing

--- src/app/components/parent-data/parent-data.component.ts
+++ src/app/components/parent-data/parent-data.component.ts
@@ -3,15 +3,13 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-parent-data',
   templateUrl: './parent-data.component.html',
-  styleUrls: ['./parent-data.component.css']
+  styleUrls: ['./parent-data.component.css'],
 })
 export class ParentDataComponent implements OnInit {
+  @Input() userName: string = '';
+  @Input() userData!: { email: string; role: string };
 
-  @Input() name: string = '';
-  @Input() userData!: {email: string, role: string}
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
